b7a3664bc21fb7752e3ca88b0cfb19b2dc32961a
aborisihin <a.borisihin@gmail.com>
2018-08-21 17:20:23 +0300

    dt - wip

--- ml_models_implementation/decision_tree/dt_class.py
+++ ml_models_implementation/decision_tree/dt_class.py
@@ -65,7 +65,7 @@ def _node_value_regression(y):
 
 
 # Словарь функций вычисления оветов в узле
-_leaf_value_dict = {'classification': _node_value_classification,
+_node_value_dict = {'classification': _node_value_classification,
                     'regression': _node_value_regression}
 
 
@@ -84,7 +84,7 @@ def _node_labels_ratio_regression(y, n_classes):
 
 
 # Словарь функций вычисления вероятностных оветов в узле
-_leaf_labels_ratio_dict = {'classification': _node_labels_ratio_classification,
+_node_labels_ratio_dict = {'classification': _node_labels_ratio_classification,
                            'regression': _node_labels_ratio_regression}
 
 
@@ -94,28 +94,27 @@ _leaf_labels_ratio_dict = {'classification': _node_labels_ratio_classification,
 
 class _TreeNode():
     """ Tree node class
-    Класс узла дерева
+    Класс узла дерева.
 
     Parameters
     ----------
     feature_idx : int, default: None
-        Индекс признака разбиения в узле
+        Индекс признака разбиения в узле.
 
     threshold : float, default: None
-        Значение порога разбиения в узле
+        Значение порога разбиения в узле.
 
     node_value : float, default: None
-        Ответ в узле
+        Ответ в узле.
 
     node_labels_ratio : float, default: None
-        Вероятностный ответ в узле
-        (распределение классов в задаче классификации)
+        Вероятностный ответ в узле (распределение классов в задаче классификации).
 
     left_child : _TreeNode, default: None
-        Левый дочерний узел
+        Левый дочерний узел.
 
     right_child : _TreeNode, default: None
-        Правый дочерний узел
+        Правый дочерний узел.
     """
     def __init__(self, feature_idx=None, threshold=None,
                  node_value=None, node_labels_ratio=None,
@@ -135,74 +134,145 @@ DecisionTree class
 
 
 class DecisionTree(BaseEstimator):
-    """ DecisionTree classifier/regressor class
+    """ DecisionTree classifier/regressor class.
     Класс реализует модель дерева решений, поддерживающего задачи классификации и регрессии.
 
     Parameters
     ----------
-    tags : [string]
-        Список допустимых для классификации тегов (классов). Не входящие в этот список теги
-        игнорируются.
+    max_depth : int or None, default: None
+        Максимальная глубина построения дерева.
+        Если не задано, максимальная глубина не органичена.
+
+    min_samples_split : int, default: 2
+        Минимальное количество примеров обучающей выборки в узле, при котором происходит 
+        ее разбиение.
+
+    criterion : string, default: 'gini'
+        Тип критерия разбиения. Поддерживаемые значения:
+        - для задачи классификации: 
+            'gini' (неопределенность Джини)
+            'entropy' (энтропия Шеннона)
+        - для задачи регрессии:
+            'variance' (дисперсия вокруг среднего)
+            'mad_median' (среднее отклонение от медианы).
+
+    verbose : bool, default: False
+        Флаг вывода сообщений в консоль.
 
     Attributes
     ----------
-    vocab_ : dict {string: int}
-        Mapping слов-признаков в численные индексы. Слова добавляются в словарь в процессе обучения,
-        индексы назначаются инкрементально.
-        Т.к. обучение модели ведется по онлайн-схеме, то всего признакового пространства мы не знаем.
-        В этом случае пользоваться bag-of-words или, например, CountVectorizer из sklearn, не
-        целесообразно (словарь придется пересчитывать при каждом появлении нового слова).
-    """
-    def __init__(self, max_depth=np.inf, min_samples_split=2,
-                 criterion='gini', debug=False):
+    _criterion_func : function
+        Функция подсчета критерия разбиения.
 
-        params = {'max_depth': max_depth,
+    _node_value : function
+        Функция вычисления ответа в узле.
+
+    _node_labels_ratio : function
+        Функция вычисления вероятностного ответа в узле.
+
+    _n_classes : int or None
+        Количество классов в задаче классификации (не определен для задачи регрессии).
+
+    _root : _TreeNode
+        Корневой узел построенного дерева.
+    """
+    def __init__(self, max_depth=None, min_samples_split=2,
+                 criterion='gini', verbose=False):
+
+        params = {'max_depth': max_depth or np.inf,
                   'min_samples_split': min_samples_split,
                   'criterion': criterion,
-                  'debug': debug}
+                  'verbose': verbose}
 
         self.set_params(**params)
 
     def set_params(self, **params):
+        """Set parameters for model.
+        Метод установки параметров модели. Переопределенный метод родительского класса
+        sklearn.base.BaseEstimator.
 
+        Returns
+        -------
+        self : object
+            Возвращает объект класса.
+        """
         super(DecisionTree, self).set_params(**params)
 
-        self._criterion = _criterion_dict[self.criterion]
-        self._leaf_value = _leaf_value_dict[_criterion_types_dict[self.criterion]]
-        self._leaf_labels_ratio = _leaf_labels_ratio_dict[_criterion_types_dict[self.criterion]]
+        self._criterion_func = _criterion_dict[self.criterion]
+        self._node_value = _node_value_dict[_criterion_types_dict[self.criterion]]
+        self._node_labels_ratio = _node_labels_ratio_dict[_criterion_types_dict[self.criterion]]
 
-        if self.debug:
+        if self.verbose:
             print('DecisionTree class set params:')
             print(params)
 
         return self
 
     def _functional(self, X, y, feature_idx, threshold):
+        """Get functional value.
+        Вычисление значения функционала в узле.
 
+        Parameters
+        ----------
+        X : array-like, shape = [n_sub_samples, n_features]
+            Массив примеров обучающей выборки, находящихся в узле.
+
+        y : array-like, shape = [n_sub_samples] or [n_sub_samples, n_classes]
+            Вектор целевых значений для примеров из узла.
+
+        feature_idx : int
+            Индекс признака, по которому проводится разбиение в узле.
+
+        threshold : float
+            Порог разбиения выборки по признаку.
+
+        Returns
+        -------
+        float
+            Возвращает вычисленное значение функционала в узле.
+        """
         division_mask = (X[:, feature_idx] < threshold)
         X_size = X.shape[0]
         l_size = np.sum(division_mask.astype(int))
         r_size = X_size - l_size
 
         if X_size != 0:
-            return (self._criterion(y) -
-                    (l_size / X_size) * self._criterion(y[division_mask]) -
-                    (r_size / X_size) * self._criterion(y[~division_mask]))
+            return (self._criterion_func(y) -
+                    (l_size / X_size) * self._criterion_func(y[division_mask]) -
+                    (r_size / X_size) * self._criterion_func(y[~division_mask]))
         else:
             return 0.0
 
     def _build_tree(self, X, y, depth=1):
+        """Build a tree.
+        Рекурсивный метод формирования узлов дерева решения.
 
+        Parameters
+        ----------
+        X : array-like, shape = [n_sub_samples, n_features]
+            Массив примеров обучающей выборки, находящихся в узле.
+
+        y : array-like, shape = [n_sub_samples] or [n_sub_samples, n_classes]
+            Вектор целевых значений для примеров из узла.
+
+        depth : int, default: 1
+            Значение глубины узла, от которого строится текущее дерево.
+
+        Returns
+        -------
+        _TreeNode
+            Возвращает корневой узел построенного дерева.
+        """
         if np.unique(y).shape[0] == 1:
 
-            if self.debug:
-                l_ratio = self._leaf_labels_ratio(y, self._n_classes)
+            if self.verbose:
+                l_ratio = self._node_labels_ratio(y, self._n_classes)
                 print('create leaf (depth={} n_objects={}): value = {} labels_ratio = {}'.format(
-                    depth, X.shape[0], round(self._leaf_value(y), 2),
+                    depth, X.shape[0], round(self._node_value(y), 2),
                     np.round(l_ratio, decimals=2) if l_ratio is not None else None))
 
-            return _TreeNode(leaf_value=self._leaf_value(y),
-                             leaf_labels_ratio=self._leaf_labels_ratio(y, self._n_classes))
+            return _TreeNode(node_value=self._node_value(y),
+                             node_labels_ratio=self._node_labels_ratio(y, self._n_classes))
 
         best_functional = 0.0
         best_feature_idx = None
@@ -229,7 +299,7 @@ class DecisionTree(BaseEstimator):
 
         if best_feature_idx is not None:
 
-            if self.debug:
+            if self.verbose:
                 print('node (depth={} n_objects={}) division: feature_idx = {} threshold = {}'.format(
                     depth, X.shape[0], best_feature_idx, round(best_threshold, 2)))
 
@@ -241,17 +311,32 @@ class DecisionTree(BaseEstimator):
 
         else:
 
-            if self.debug:
-                l_ratio = self._leaf_labels_ratio(y, self._n_classes)
+            if self.verbose:
+                l_ratio = self._node_labels_ratio(y, self._n_classes)
                 print('create leaf (depth={} n_objects={}): value = {} labels_ratio = {}'.format(
-                    depth, X.shape[0], round(self._leaf_value(y), 2),
+                    depth, X.shape[0], round(self._node_value(y), 2),
                     np.round(l_ratio, decimals=2) if l_ratio is not None else None))
 
-            return _TreeNode(leaf_value=self._leaf_value(y),
-                             leaf_labels_ratio=self._leaf_labels_ratio(y, self._n_classes))
+            return _TreeNode(node_value=self._node_value(y),
+                             node_labels_ratio=self._node_labels_ratio(y, self._n_classes))
 
     def fit(self, X, y):
+        """Fit a model.
+        Метод обучения модели (построения дерева решений).
 
+        Parameters
+        ----------
+        X : array-like, shape = [n_samples, n_features]
+            Массив примеров обучающей выборки.
+
+        y : array-like, shape = [n_samples] or [n_samples, n_classes]
+            Вектор целевых значений для примеров обучающей выборки.
+
+        Returns
+        -------
+        self : object
+            Возвращает объект класса.
+        """
         if _criterion_types_dict[self.criterion] == 'classification':
             self._n_classes = np.amax(y) + 1
         else:
@@ -262,10 +347,22 @@ class DecisionTree(BaseEstimator):
         return self
 
     def _get_object_leaf(self, obj):
+        """Get a leaf node for object.
+        Метод получения листового узла для примера из тестовой выборки.
 
+        Parameters
+        ----------
+        obj : array-like, shape = [n_features]
+            Пример из тестовой выборки.
+
+        Returns
+        -------
+        _TreeNode
+            Возвращает листовой узел дерева.
+        """
         node = self._root
 
-        while node.leaf_value is None:
+        while node.node_value is None:
 
             if obj[node.feature_idx] < node.threshold:
                 node = node.left_child
@@ -275,13 +372,69 @@ class DecisionTree(BaseEstimator):
         return node
 
     def _predict_object(self, obj):
-        return self._get_object_leaf(obj).leaf_value
+        """Get a prediction for object.
+        Метод получения прогноза для примера из тестовой выборки.
+
+        Parameters
+        ----------
+        obj : array-like, shape = [n_features]
+            Пример из тестовой выборки.
+
+        Returns
+        -------
+        int or float
+            Возвращает предсказание для примера (индекс класса для задачи классификации
+            или вещественное значение для задачи регрессии).
+        """
+        return self._get_object_leaf(obj).node_value
 
     def _predict_object_proba(self, obj):
-        return self._get_object_leaf(obj).leaf_labels_ratio
+        """Get a probability prediction for object.
+        Метод получения вероятностного прогноза для примера из тестовой выборки.
+
+        Parameters
+        ----------
+        obj : array-like, shape = [n_features]
+            Пример из тестовой выборки.
+
+        Returns
+        -------
+        array-like, shape = [n_classes]
+            Возвращает вектор вероятностного предсказания для примера (вероятности 
+            принадлежности объекта каждому классу).
+        """
+        return self._get_object_leaf(obj).node_labels_ratio
 
     def predict(self, X):
+        """Get a prediction for list of objects.
+        Метод получения вероятностного прогноза для тестовой выборки.
+
+        Parameters
+        ----------
+        X : array-like, shape = [n_samples, n_features]
+            Массив примеров из тестовой выборки.
+
+        Returns
+        -------
+        array-like, shape = [n_samples]
+            Возвращает вектор предсказаний для выборки (индекс класса для задачи 
+            классификации или вещественное значение для задачи регрессии).
+        """
         return np.array([self._predict_object(obj) for obj in X])
 
     def predict_proba(self, X):
+        """Get a probability prediction for list of objects.
+        Метод получения вероятностного прогноза для тестовой выборки.
+
+        Parameters
+        ----------
+        X : array-like, shape = [n_samples, n_features]
+            Массив примеров из тестовой выборки.
+
+        Returns
+        -------
+        array-like, shape = [n_samples, n_classes]
+            Возвращает массив вероятностных предсказаний для выборки (вероятности 
+            принадлежности объектов каждому классу).
+        """
         return np.array([self._predict_object_proba(obj) for obj in X])
